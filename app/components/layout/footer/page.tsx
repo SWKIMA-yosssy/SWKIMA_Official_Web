4178ed24a983e0c56cfa3bd9bbc00995aa4d887b
SWKIMA-arch <greyonth@gmail.com>
2025-04-24 21:43:14 +0900

    create header and footer

---
+++ app/components/layout/footer/page.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <footer>
+      <small>Copyright Ⓒ+ⓅSwkima. All Rights Reserved.</small>
+    </footer>
+  )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>contact
+      </h2>
+      <div>
+        <h3>CONTACT</h3>
+        <p>swkima.contact@gmail.com</p>
+      </div>
+      <div>
+        <h3>MANEGEMENT</h3>
+        <p>現在、楽曲をリリースできるレーベルを探しています<br />
+          ご興味のあるオーナー様はぜひ上記メールアドレスにご一報くださいませ
+        </p>
+      </div>
+    </div>
+  )
+}
