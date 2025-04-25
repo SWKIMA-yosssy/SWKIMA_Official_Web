70f0a843781f565117997e341689b4471d97bcfd
SWKIMA-arch <greyonth@gmail.com>
2025-04-25 23:01:37 +0900

    avoid pure selecta problem in module css

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,12 +3,12 @@ export default function Page() {
   return (
     <div className={styles.contact}>
       <div>
-        <h1>CONTACT</h1>
-        <p>swkima.contact@gmail.com</p>
+        <h1 className={styles.title}>CONTACT</h1>
+        <p className={styles.statements}>swkima.contact@gmail.com</p>
       </div>
       <hr />
       <div>
-        <h1>MANEGEMENT</h1>
+        <h1 className={styles.title}>MANEGEMENT</h1>
         <p>現在、楽曲をリリースできるレーベルを探しています<br />
           ご興味のあるオーナー様はぜひ上記メールアドレスにご一報くださいませ
         </p>
