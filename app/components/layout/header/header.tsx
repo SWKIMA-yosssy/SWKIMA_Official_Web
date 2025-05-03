b68515005d1549ea880cae40dcf30079651c6497
SWKIMA-arch <greyonth@gmail.com>
2025-05-03 16:56:16 +0900

    revice header nav and add soundcloud link in SNSLinks

--- app/components/layout/header/header.tsx
+++ app/components/layout/header/header.tsx
@@ -1,10 +1,18 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link"
 import Image from "next/image"
 import styles from "./header.module.css"
 export default function Header() {
+  const pathname = usePathname();
+  const navLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/works', label: 'Works' },
+    { href: '/contact', label: 'Contact' },
+  ]
 
   return (
-    <header className={styles.header}>
+    <header className="w-full m-2">
       <nav className={styles.headerNav}>
 
         <div className={styles.logoWrapper}>
@@ -20,9 +28,18 @@ export default function Header() {
         </div>
 
         <div className={styles.navWrapper}>
-          <Link className="text-xl hover:underline duration-1000 ease-linear " href="/">Home</Link>
-          <Link className="text-xl hover:underline" href="/works">Works</Link>
-          <Link className="text-xl hover:underline" href="/contact">Contact</Link>
+          {navLinks.map(({ href, label }) => {
+            const isActive = pathname == href;
+            return (
+              <Link
+                key={href}
+                href={href}
+                className={`text-xl duration-500 ease-linear
+                  ${isActive ? 'text-muted-foreground line-through' : 'visible'}`}
+              >{label}</Link>
+            )
+          }
+          )}
         </div>
       </nav>
     </header >
