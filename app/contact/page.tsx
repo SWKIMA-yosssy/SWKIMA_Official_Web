857d577204c361a0f121731eb4100d3e59a0d6f0
SWKIMA-arch <greyonth@gmail.com>
2025-05-05 15:34:15 +0900

    rewrite the Hero section and Contact page by completely TailwindCSS

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,19 @@
-import styles from "./page.module.css"
 export default function Page() {
   return (
-    <div className={styles.contact}>
-      <div>
-        <h1 className={styles.title}>CONTACT</h1>
-        <p className={styles.statements}>swkima.contact@gmail.com</p>
+    <div className="text-center">
+      <div className="m-[100px]">
+        <h1 className="mb-12 text-2xl">CONTACT</h1>
+        <p className="text-xl">swkima.contact@gmail.com</p>
       </div>
-      <hr />
-      <div>
-        <h1 className={styles.title}>MANEGEMENT</h1>
-        <p>現在、楽曲をリリースできるレーベルを探しています<br />
-          ご興味のあるオーナー様はぜひ上記メールアドレスにご一報くださいませ
+      <hr className="text-foreground ml-[20%] mr-[20%]" />
+      <div className="m-[100px]">
+        <h1 className="mb-12 text-2xl">MANEGEMENT</h1>
+        <p>現在、楽曲をリリースしていただけるレーベル様を探しております。<br />
+          ご興味をお持ちのレーベル関係者の方は、上記のメールアドレスまでご連絡いただけますと幸いです。
+        </p>
+        <p className="m-3">
+          I am currently looking for a label to release my music. <br />
+          If you are interested, I would greatly appreciate it if you could contact me via the email address above.
         </p>
       </div>
     </div>
